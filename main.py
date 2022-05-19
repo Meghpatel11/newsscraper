56aded9d0bd480a82f160ac9c3dad8f9fd0eaa58
megh <megh@gitgmail.com>
2022-05-18 18:09:26 -0700

    comments added

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ url = "https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blackl
 result = requests.get(url)
 data = BeautifulSoup(result.text,'html.parser')
 
-
+#getting required data
 title = data.find('h1').text
 content = data.find('div', class_="grid-body").text[38:-62].split('.')
 
+#print results
 print(title)
 
 for lines in content:
