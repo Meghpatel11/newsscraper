0041642add74fe3a980532dd29ed1ad79c4a7d26
megh <megh@gitgmail.com>
2022-05-18 18:04:12 -0700

    Project compeleted

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.washingtonpost.com/technology/2020/09/25/privacy-check-blacklight"
+
+#make request 
+result = requests.get(url)
+data = BeautifulSoup(result.text,'html.parser')
+
+
+title = data.find('h1').text
+content = data.find('div', class_="grid-body").text[38:-62].split('.')
+
+print(title)
+
+for lines in content:
+    print(lines)
+
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+content = data.find('div', class_="grid-body").text[38:-62]
